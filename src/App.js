2bef37f591c42582b6c4f1bf9261ed63e4486b13
david-shakaya <devidoz666@gmail.com>
2021-01-24 18:09:40 +0200

    + cart prod and props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import ProductCart from './components/Product-card'
 
+//  Обект со свойствами (ОН ДОЛЖЕН НАХОДИТЬСЯ В ДРУГОМ ФАЙЛЕ но пока так)
+ const obj={
+    id: "id-1",
+    url: "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
+    title: "Feathers. Art abstract",
+    price: 500,
+    author: {
+      tag: "ractapopulous",
+      url: "https://pixabay.com/users/ractapopulous-24766/"
+    },
+    quantity: 10
+ }
 const App = () => {
     return (
         <div>
             <h1>Главный компонент-контейнер страницы</h1>
-            <ProductCart/>
+            <ProductCart
+                src={obj.url}
+                alt="Feathers. Art abstract"
+                title={obj.title}
+                price={obj.price}
+            />
+            <ProductCart
+            src={obj.url}
+                alt="Feathers. Art abstract"
+                title={obj.title}
+                price={obj.price}
+            />
         </div>
     )
 }
--- src/components/Product-card.js
+++ src/components/Product-card.js
@@ -1,24 +1,13 @@
 import React from 'react'
-//  Обект со свойствами (ОН ДОЛЖЕН НАХОДИТЬСЯ В ДРУГОМ ФАЙЛЕ но пока так)
- const obj={
-    id: "id-1",
-    url: "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
-    title: "Feathers. Art abstract",
-    price: 500,
-    author: {
-      tag: "ractapopulous",
-      url: "https://pixabay.com/users/ractapopulous-24766/"
-    },
-    quantity: 10
- }
+
 //  Шаблон в который подставляем даннные с обьекта
-const ProductCart = () => {
+const ProductCart = (props) => {
     return (<div>
-  <img src={obj.url} alt={obj.title} width="300px" />
-  <h2>{obj.title}</h2>
-  <p>Автор: <a href={obj.author.url}> {obj.author.tag} </a></p>
-  <p>Цена {obj.price} кредитов</p>
-  <button type="button">Добавить в корзину</button>
+  <img src={props.src} alt={props.alt} width="300px" />
+  <h2>{props.title}</h2>
+  {/* <p>Автор: <a href={obj.author.url}> {obj.author.tag} </a></p> */}
+  <p>Цена {props.price} кредитов</p>
+  <button type="button">Добавить в корзину</button> 
 </div>)
 }
 
